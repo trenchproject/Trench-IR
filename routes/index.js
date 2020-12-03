5450f3e347fb7d52debd32b8ee3fc9d485d25b5c
avmey <avmey98@gmail.com>
2020-12-03 16:02:40 -0500

    populating metadata

--- routes/index.js
+++ routes/index.js
@@ -46,7 +46,6 @@ router.get('/gallery', async (req, res, next) => {
 
     for await (const blob of listBlobsResponse.segment.blobItems) {
       console.log(`Blob: ${blob.name}`);
-      blob.getProperties();
     }
 
     viewData = {
