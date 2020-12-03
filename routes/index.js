342524f8d66365a1125f54464b9a267935dfc43d
avmey <avmey98@gmail.com>
2020-12-03 17:21:06 -0500

    populating metadata

--- routes/index.js
+++ routes/index.js
@@ -167,7 +167,7 @@ router.post('/', uploadStrategy, async (req, res) => {
 
 router.get('/page', async (req, res, next) => {
   try {
-    let viewData = {name:'',fauna1=''};
+    let viewData = {name:'',fauna1:''};
     viewData.name = req.query.name;
     viewData.fauna1 = req.query.fauna1;
     res.render('page', viewData);
