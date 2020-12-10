9cef1058b4473b525b1f9789b3f8539883226363
avmey <avmey98@gmail.com>
2020-12-10 15:16:37 -0500

    Changed gallery to iron images

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,9 @@ router.get('/gallery', async (req, res, next) => {
 
     for await (const blobOG of listBlobsResponseOG.segment.blobItems) {
       for await (const blobUP of listBlobsResponseUP.segment.blobItems) {
-        if(blobOG.name.splice(5,0) == blobUP.name){
+        let n = blobOG.name;
+        n = n.splice(5,0);
+        if(n == blobUP.name){
           blobOG.metadata = blobUP.metadata;
           blobOG.name = blobUP.name;
         }
