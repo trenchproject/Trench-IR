5657c22746d039ab74ede411ff31e577281d7429
Abby Meyer <ameyer@Abbys-MacBook-Pro.local>
2022-01-28 15:48:53 -0500

    added pollination educational resource

--- routes/index.js
+++ routes/index.js
@@ -156,6 +156,11 @@ router.get('/king-penguins', async (req, res, next) => {
   res.render('king-penguins');
 });
 
+// No blob data needed for pollination page 
+router.get('/pollination', async (req, res, next) => {
+  res.render('pollination');
+});
+
 // No blob data needed for koala page 
 router.get('/koalas', async (req, res, next) => {
   res.render('koalas');
